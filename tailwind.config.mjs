d470001bcf984327a80ba1f39d22a78ef3542cc0
LiamWalshTech <admin@liamwalsh.tech>
2024-07-29 21:07:59 +0100

    Adding theme config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
+  theme: {
+    extend: {
+      colors: {
+        'candy-apple-red': '#FC2E20',
+        'safety-orange': '#FD7F20',
+        'amber': '#FFBF00',
+        'vivid-sky-blue': '#00D8FF',
+        'deep-sky-blue': '#00BFFF',
+        'picton-blue': '#00A7FF',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+  plugins: [],
 }
